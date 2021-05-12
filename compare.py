e474863f3b4d86adeea610af843d4b6c09666ec4
Severyn12 <73779019+Severyn12@users.noreply.github.com>
2021-05-12 19:41:53 +0300

    Update compare.py

--- compare.py
+++ compare.py
@@ -2,7 +2,7 @@
 from bin_tree import LinkedBST
 
 bin_tree = LinkedBST()
-times = bin_tree.demo_bst('2.txt')
+times = bin_tree.demo_bst('words.txt')
 print('Words are in the alphabetic order\n')
 print(f'Search time of 10 000 words in list: {times[0]}\n')
 print(f'Search time of 10 000 words in bin tree: {times[1]}\n')
